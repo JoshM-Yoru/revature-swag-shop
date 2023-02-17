740bf742bbbbd80fef670b27c17b0623cbc3ce0e
JoshM-Yoru <jmaciel335i@gmail.com>
2023-02-17 13:12:02 -0500

    added jest testing

--- frontend-rss/src/__test__/Login.test.tsx
+++ frontend-rss/src/__test__/Login.test.tsx
@@ -2,5 +2,6 @@ import Login from "../components/forms/Login";
 import { render } from "@testing-library/react";
 
 test('Renders component correctly', () => {
+    render(<Login />)
     expect(true).toBeTruthy();
 })
